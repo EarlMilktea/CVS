a8503b95aca623fc9ee961335b34da76f7786e3d
Sora Shiratani <hclfuranpon_compotayamano_0117@outlook.jp>
2023-01-15 09:00:48 +0900

    Restore tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,8 +9,7 @@ function _logΠ_gauss(x)
 end
 
 function _test_sampler(kernel, d, ns; ninit = 10000, atol = 0.025)
-    # x = d ≠ 1 ? zeros(d) : 0.0
-    x = zeros(d)
+    x = d ≠ 1 ? zeros(d) : 0.0
     for _ ∈ 1:ninit
         x = kernel(x)
     end
@@ -35,16 +34,16 @@ end
 
 @testset "HMC" begin
     let rng = MersenneTwister(0)
-        @test _test_sampler(x -> hmc(_logΠ_gauss, x, 1; rng = rng), 1, 1000000)
-        @test _test_sampler(x -> hmc(_logΠ_gauss, x, 1; rng = rng), 2, 1000000)
-        @test _test_sampler(x -> hmc(_logΠ_gauss, x, 1; rng = rng), 5, 1000000)
+        @test _test_sampler(x -> hmc(_logΠ_gauss, x, 1; rng = rng), 1, 500000)
+        @test _test_sampler(x -> hmc(_logΠ_gauss, x, 1; rng = rng), 2, 500000)
+        @test _test_sampler(x -> hmc(_logΠ_gauss, x, 1; rng = rng), 5, 500000)
     end
 end
 
 @testset "NUTS" begin
     let rng = MersenneTwister(0)
-        @test _test_sampler(x -> nuts(_logΠ_gauss, x, 0.1; rng = rng), 1, 1000000)
-        @test _test_sampler(x -> nuts(_logΠ_gauss, x, 0.1; rng = rng), 2, 1000000)
-        @test _test_sampler(x -> nuts(_logΠ_gauss, x, 0.1; rng = rng), 5, 1000000)
+        @test _test_sampler(x -> nuts(_logΠ_gauss, x, 0.1; rng = rng), 1, 300000)
+        @test _test_sampler(x -> nuts(_logΠ_gauss, x, 0.1; rng = rng), 2, 300000)
+        @test _test_sampler(x -> nuts(_logΠ_gauss, x, 0.1; rng = rng), 5, 300000)
     end
 end
